a26cd5e0f17c02cbc7c16897dc816c1305b28669
aminaryan80 <mohammadamin.aryan@divar.ir>
2022-06-15 18:11:13 +0430

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Mohammadamin Aryan on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
