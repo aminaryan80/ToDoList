99fef7ea90ba6acc25c6b917400740e4e6b59258
aminaryan80 <mohammadamin.aryan@divar.ir>
2022-06-15 18:33:09 +0430

    Init

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,11 +8,107 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        UITabBar.appearance().barTintColor = .systemBackground
+        UINavigationBar.appearance().barTintColor = .systemBackground
+    }
+    
+    @State var selectedIndex = 0
+    @State var shouldShowModal = false
+    
+    let tabBarImageNames = ["person", "gear", "plus.app.fill", "pencil", "lasso"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 0) {
+            
+            ZStack {
+                
+                Spacer()
+                    .fullScreenCover(isPresented: $shouldShowModal, content: {
+                        Button(action: {shouldShowModal.toggle()}, label: {
+                            Text("Fullscreen cover")
+                        })
+                    
+                })
+                
+                switch selectedIndex {
+                case 0:
+                    NavigationView {
+                        ScrollView {
+                            ForEach(0..<100) { num in
+                                Text("\(num)")
+                            }
+                        }
+                            .navigationTitle("First Tab")
+                    }
+                    
+                case 1:
+                    ScrollView {
+                        Text("TEST")
+                    }
+                    
+                default:
+                    NavigationView {
+                        Text("Remaining tabs")
+                        
+                    }
+                }
+                
+            }
+            
+//            Spacer()
+            
+            Divider()
+                .padding(.bottom, 8)
+            
+            HStack {
+                ForEach(0..<2) { num in
+                    Button(action: {
+                        
+                        if num == 2 {
+                            shouldShowModal.toggle()
+                            return
+                        }
+                        
+                        selectedIndex = num
+                    }, label: {
+                        Spacer()
+                        
+                        if num == 2 {
+                            Image(systemName: tabBarImageNames[num])
+                                .font(.system(size: 44, weight: .bold))
+                                .foregroundColor(.red)
+                        } else {
+                            Image(systemName: tabBarImageNames[num])
+                                .font(.system(size: 24, weight: .bold))
+                                .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
+                        }
+                        
+                        
+                        Spacer()
+                    })
+                    
+                }
+            }
+            
+            
+        }
     }
 }
+
+//TabView {
+//    Text("First")
+//        .tabItem {
+//            Image(systemName: "person")
+//            Text("First")
+//        }
+//    Text("Second")
+//        .tabItem {
+//            Image(systemName: "gear")
+//            Text("Second")
+//        }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
